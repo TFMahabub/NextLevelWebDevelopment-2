a12f23f1d04dc140c0c9ecb46aa1a196f0b40198
Mahabub Alam <rjmahabub543@gmail.com>
2023-05-17 18:48:10 +0600

    finish 6-4

--- module-6 (mongoose)/src/app.ts
+++ module-6 (mongoose)/src/app.ts
@@ -6,15 +6,94 @@ import {
   Request,
   Response,
 } from "express-serve-static-core";
+import { Schema } from "mongoose";
 
 const app: Application = express();
 
 //useing cors-
 app.use(cors());
 
+//parse data-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-  next();
+  //   res.send("Hello World!");
+  //   next();
+
+  // 1. Create an interface representing a document in MongoDB.
+  interface IUser<userSchema> {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergencyContactNumber: string;
+    permanentAddress: string;
+  }
+
+  // 2. Create a Schema corresponding to the document interface.
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+        required: false,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    emergencyContactNumber: {
+      type: String,
+      required: true,
+    },
+    permanentAddress: {
+      type: String,
+      required: true,
+    },
+  });
 });
 
 export default app;
